9324762a7835269ec764a832d59441627ddcaff6
crowelmk <crowelmk@rose-hulman.edu>
2019-04-02 20:21:04 -0400

    Basic shell script to clone relevant code repos into current directory

---
+++ install_repos.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+git clone https://github.com/RHIT-XPrize/rhit-xprize-pipeline.git
+git clone https://github.com/RHIT-XPrize/PointingAPI.git
+git clone https://github.com/RHIT-XPrize/annotator-server.git
