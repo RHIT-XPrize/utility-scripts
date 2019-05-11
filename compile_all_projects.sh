52201ac72b5b5bef683fbb0b0721011f100f853d
crowelmk <crowelmk@rose-hulman.edu>
2019-05-11 00:26:57 -0400

    Fixed running C# app from command-line

--- compile_all_projects.sh
+++ compile_all_projects.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 gradle build -p rhit-xprize-pipeline
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" PointingAPI/KinectPointingAPI.sln -t:restore
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" PointingAPI/KinectPointingAPI.sln
--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 printf "Starting up annotator web hosts...\n"
 start python annotator-server/app.py
-#start PointingAPI/KinectPointingAPI/bin/Debug/app.exe
+start "C:\Program Files (x86)\IIS Express\iisexpress.exe" -config:PointingAPI/.vs/config/applicationhost.config
 sleep 5
 printf "Running UIMA pipeline.\n"
 gradle run -p rhit-xprize-pipeline
